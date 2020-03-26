0266edeee65e181b0f98835832643ec4b454cbd8
Zack Poorman <43649301+ZackPoorman@users.noreply.github.com>
2020-03-26 16:56:53 -0400

    Slightly improves but needs longer testing

--- TrainingAlg.py
+++ TrainingAlg.py
@@ -1,10 +1,10 @@
-import retro
-import numpy as np
-import cv2
-import neat
-import pickle
+import retro        #pip install gym-retro
+import numpy as np  #pip install numpy
+import cv2          #pip install opencv-python==4.1.2.30
+import neat         #pip install neat-python
+import pickle       #pip install pickle
 
-env = retro.make('Arkanoid-Nes', 'Level1')
+env = retro.make('BalloonFight-Nes', 'Level1')
 imgarray = []
 def eval_genomes(genomes, config):
     for genome_id, genome in genomes:
@@ -31,9 +31,18 @@ def eval_genomes(genomes, config):
         score_max = 0
         done = False
 
+
+        #Uncomment to show what ANN sees
+        #cv2.namedWindow("main", cv2.WINDOW_NORMAL)
+
         while not done:
+            #comment out to show what ANN sees
             env.render()
+            
             frame += 1
+            #Scaledimg is used for showing ANN visually
+            scaledimg = cv2.cvtColor(ob, cv2.COLOR_BGR2GRAY)
+            scaledimg = cv2.resize(scaledimg, (iny, inx))
 
             #Actually resizes the screenshot input
             ob = cv2.resize(ob, (inx, iny))
@@ -42,9 +51,12 @@ def eval_genomes(genomes, config):
             #Resize the input for the ANN
             ob = np.reshape(ob, (inx, iny))
 
-            for x in ob:
-                for y in x:
-                    imgarray.append(y)
+            #Uncomment to show what ANN sees
+            #cv2.imshow('main', scaledimg)
+            #cv2.waitKey(1)
+            
+            #Flatten screen for input
+            imgarray = np.ndarray.flatten(ob)
             
             nnOutput = net.activate(imgarray)
 
@@ -53,13 +65,18 @@ def eval_genomes(genomes, config):
 
             score = info['score']
 
+            #For a basic neat alg comment this if statement
             if score > score_max:
                 fitness_current += 1
                 score_max = score
+            
+            #Uncomment this for the basic neat alg
+            #fitness_current += rew
 
             if fitness_current > current_max_fitness:
                 current_max_fitness = fitness_current
                 counter = 0
+                fitness += 1
             else:
                 counter += 1
 
@@ -69,11 +86,21 @@ def eval_genomes(genomes, config):
 
             genome.fitness = fitness_current
 
-
 config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
                      neat.DefaultSpeciesSet, neat.DefaultStagnation,
                      'config-feedforward')
 
+#Creates the population based on the config file
 p = neat.Population(config)
 
+#Creates statistics for each generation
+p.add_reporter(neat.StdOutReporter(True))
+stats = neat.StatisticsReporter()
+p.add_reporter(stats)
+p.add_reporter(neat.Checkpointer(10))
+
 winner = p.run(eval_genomes)
+
+#Saves current ANN
+with open('winner.pkl', 'wb') as output:
+    pickle.dump(winner, output, 1)
