c780ea676824d579d1692e66a2dcdb2bc591e25b
Zack Poorman <43649301+ZackPoorman@users.noreply.github.com>
2020-03-26 20:33:45 -0400

    Fixed an issue of incrementing fitness level before it was assigned

--- TrainingAlg.py
+++ TrainingAlg.py
@@ -77,7 +77,7 @@ def eval_genomes(genomes, config):
             if fitness_current > current_max_fitness:
                 current_max_fitness = fitness_current
                 counter = 0
-                fitness += 1
+                #fitness += 1
             else:
                 counter += 1
 
